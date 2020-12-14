f5931ec424e7c8d7a5460e44e373236d055a350f
paradeisios <p.boulakis@student.maastrichtuniversity.nl>
2020-12-14 13:11:01 +0200

    Fixed RT creation

--- data_processor.py
+++ data_processor.py
@@ -1,136 +1,99 @@
-   
-# =============================================================================
-# HAVE ALL LOG FILES IN THE SAME FOLDER    
-# =============================================================================
-
-import os
+import os 
+from os.path import join
 import pandas as pd
+import numpy as np
 
-def  processor(filename):
-   
-    ####### open  file and get lines with condition and response key #######
-    
-    file = open(filename,'r')
-    data = []
-    
-    for line in file :
-        if ('MINEOTHER' in line) and ('Finished' in line):
+log_dir = "/home/paradeisios/Desktop/Individual_Logs/"
+output_dir = "/home/paradeisios/Desktop/"
+
+mapping = dict(GS248="sub_01",
+               CM249="sub_02",
+               WT250="sub_03",
+               XA251="sub_04",
+               GS252="sub_05",
+               LT253="sub_06",
+               MU254="sub_07",
+               CG255="sub_08",
+               PJ256="sub_09",
+               EM257="sub_10",
+               YJ258="sub_11",
+               GA259="sub_12",
+               AF260="sub_13",
+               MT261="sub_14",
+               CL262="sub_15",
+               CG263="sub_16",
+               TW265="sub_17",
+               BP267="sub_18",
+               CS268="sub_19",
+               TC269="sub_20")
+
+###############################################################################              
+def reaction_time_df(log):
+    # Extract feedback and response from key press lines in the log file
+    data          =[]
+    condition     =[]
+    responses     =[]
+    corrects      =[]
+    response_time =[]
+    probe_time    =[]
+    reaction_time =[]
+    subject       =[]
+
+    with open(log,"r") as file:
+        for line in file:
             data.append(line)
-    file.close()
-    
-    ###### break into conditions and responses   #######     
-    
-    condition = []
-    response = []
-    corrects = []
-    for line in data:
-        x = line.split()
-        condition.append(x[2])
-        response.append(x[6])
-     ##### get correct key responses
-        if (x[2] == 'MINEOTHER-SYNC' and x[6] == '1') or (x[2] == 'MINEOTHER-OSYNC' and x[6] == '2') :
-            corrects.append(1)
-        else:
-            corrects.append(0)
-    
-    #print(condition);print(len(condition)); print(response);print(len(response))
-    
-    ####### transform condition for dataframe #######
-                  
-    for i in range(len(condition)):
-        if condition[i] == 'MINEOTHER-SYNC':
-            condition[i] = 1
-        else:
-            condition[i] = 2
-            
-    #print(condition);print(len(condition)) 
-     
-        
-        
-        
-    ###################################################################
-        
-    ####### open the file
-    file = open(filename,'r')
-    data = []
-    for line in file:
-        data.append(line)
-    #print(data)
-    file.close()
-    
-    ####### get the line set with the last stimulus and the response
-    
-    times = []
-    for i in range(len(data)) :
-        if ('MINEOTHER' in data[i]) and ('Finished' in data[i]):
-            #print(data[i])
-            times.append(data[i-2])
-        
-        
-            index_minus = 0
+
+    for ii in range(len(data)):
+
+        if ('MINEOTHER' in data[ii]) and ('Finished' in data[ii]): # Get file line with response data
+            summ_line = data[ii].replace("\n","").rsplit("\t")
+
+            response = summ_line[-1].rsplit(" ")[-1]
+            responses.append(response) # Extract subject response(1=Mine, 2=Other)
+
+            feedback = summ_line[-1].rsplit(" ")[0].rsplit("-")[-1]
+            condition.append(feedback) # Extract feedback condition
+
+
+            if (feedback == 'SYNC' and response == '1') or (feedback == 'OSYNC' and response == '2') :
+                corrects.append("correct")
+            else:
+                corrects.append("wrong")
+
+            response_time.append((data[ii-2]).split("\t")[0]) # Extract response time
+
+            index_minus = 0 # Move backwards and find when probing was presented
             while True:
-                if 'Stim' in data[i-2-index_minus]:
-                    times.append(data[i-2-index_minus])
+                if 'Stim' in data[ii-2-index_minus]:
+                    probe_time.append((data[ii-2-index_minus]).rsplit("\t")[0])
                     break
                 else:
                     index_minus = index_minus + 1
-                    
-    ###### separate and take only times
-    
-    times_present = []
-    times_response = []
-    
-    for i in range(len(times)):
-        x = times[i].rsplit()
-        if i%2 == 0:
-            times_response.append(float(x[0]))
-        else:
-            times_present.append(float(x[0]))
-        
-    ##### calculate reaction times    
-    reaction_time = [x1 - x2 for (x1, x2) in zip(times_response, times_present)]  
 
-     
+            reaction_time = [x1 - x2 for (x1, x2) in zip(np.float_(response_time), np.float_(probe_time))] 
+
+    subject = [mapping[log[-12:-7]] for i in range(len(reaction_time))]
+
+
+    results = pd.DataFrame()
+    results['Subject'] = subject
+    results['Condition']= condition
+    results['Response'] = responses
+    results['Corrects'] = corrects
+    results['RT'] = reaction_time
+
+    return results
+
+##########################################################################
+
+df = []
+for log in os.listdir(log_dir):
+    if log.endswith("log"):
+        results = reaction_time_df(join(log_dir,log))
+        df.append(results)
+df = pd.concat(df)
+
+df.to_csv(join(output_dir,"HEART_reaction_time.csv"),index = False, header=True)
+
+
             
-    
-    ##### names
-    
-    names = []
-    for item in range(len(reaction_time)):
-        name = list(filename)
-        name = 's'+ str(int(''.join(name[2:5])) - 247)
-        names.append(name)
-    
-    
-    ##### individual dataframe
-    final_results = {}
-    final_results['Subject'] = names
-    final_results['Condition']= condition
-    final_results['Response'] = response
-    final_results['Reaction_Time'] = reaction_time
-    final_results['Corrects'] = corrects
-    
-  
-        
-    df_results = pd.DataFrame(final_results)
-    return(df_results)   
-
-
-####### loop through folder, transform into dataframe, concatenate and export #######
-def analysis(dirname):
-    
-    results = []
-    for name in os.listdir(dirname):
-        results.append(processor(name))
-    global finals
-    finals = pd.concat(results)
-    finals['Response'] = finals['Response'].str.replace('Timeout', '')
-    
-    finals.to_csv(r'C:\Users\User\Documents\Academia\Thesis\Methods\Behaviour\Individual_Logs\heart_behav_data.csv', index = False, header=True)
-    print(finals)
-    
-
-        
-        
-    
-
